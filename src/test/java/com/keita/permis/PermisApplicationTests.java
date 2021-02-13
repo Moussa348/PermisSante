37ef6096462164b92363472dcd049ed47c3b67aa
Moussa <Moussa.Keita2411@gmail.com>
2021-02-13 11:59:24 -0500

    initialization of the project

---
+++ src/test/java/com/keita/permis/PermisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keita.permis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
