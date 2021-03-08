11422f22e755391dd847641719c779fb01095e35
Moussa <Moussa.Keita2411@gmail.com>
2021-03-08 13:59:28 -0500

    Refactoring of LoginServiceTest with mockito

--- src/main/java/com/keita/permis/repository/PermitRepository.java
+++ src/main/java/com/keita/permis/repository/PermitRepository.java
@@ -4,9 +4,11 @@ import com.keita.permis.model.Permit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PermitRepository extends JpaRepository<Permit,Long> {
    List<Permit> findAllByAdministrator_Email(String email);
+   List<Permit> findAllByDateBetween(Date firstDate,Date secondDate);
 }
--- src/test/java/com/keita/permis/service/LoginServiceTest.java
+++ src/test/java/com/keita/permis/service/LoginServiceTest.java
@@ -3,6 +3,7 @@ package com.keita.permis.service;
 import com.keita.permis.dto.LoginForm;
 import com.keita.permis.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,20 +15,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginServiceTest {
 
     @Mock
-     UserRepository userRepository;
+    UserRepository userRepository;
+
+    @InjectMocks
+    private LoginService loginService;
 
     @Test
     public void login() {
         //Arrange
-        LoginForm loginForm = new LoginForm("massou@gmail.com","massou123");
-        LoginForm loginForm2 = new LoginForm("cancre@gmail.com","massou123");
+        LoginForm loginForm = new LoginForm("massou@gmail.com", "massou123");
+        LoginForm loginForm2 = new LoginForm("cancre@gmail.com", "massou123");
         //Act
         Mockito.when(userRepository.existsByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword())).thenReturn(true);
-        boolean isLogged = userRepository.existsByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword());
-        Mockito.when(userRepository.existsByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword())).thenReturn(false);
-        boolean isNotLogged = userRepository.existsByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword());
+        Mockito.when(userRepository.existsByEmailAndPassword(loginForm2.getEmail(), loginForm2.getPassword())).thenReturn(false);
         //Assert
-        assertTrue(isLogged);
-        assertFalse(isNotLogged);
+        assertTrue(loginService.login(loginForm) && !loginService.login(loginForm2) );
     }
 }
